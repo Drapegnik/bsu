c8dc417d3dffbdad0764458767a02caf908a2f62
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-04-07 17:48:59 +0300

    [decision-science : lab4] edmonds karp, task1 and task2 done!

--- decision-science/lab4/main.py
+++ decision-science/lab4/main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
-from FordFulkerson import FlowNetwork
-
-inp = file('task1.in', 'r')
+from algorithms import FlowNetwork
 
 if __name__ == '__main__':
-    g = FlowNetwork()
+    # task1
+    inp = file('task1.in', 'r')
+    g = FlowNetwork('task1.out.md')
     map(g.add_vertex, inp.readline().split())
     m = int(inp.readline())
     for _ in range(m):
         x, y, w = inp.readline().split()
         g.add_edge(x, y, int(w))
-    print g.max_flow('s', 't')
+    print 'task1 max flow = {}'.format(g.ford_fulkerson('s', 't'))
+
+    # task2
+    inp = file('task2.in', 'r')
+    g = FlowNetwork('task2.out.md')
+    map(g.add_vertex, inp.readline().split())
+    m = int(inp.readline())
+    for _ in range(m):
+        x, y, w = inp.readline().split()
+        g.add_edge(x, y, float(w))
+    print 'task2 max flow = {}'.format(g.edmonds_karp('s', 't'))
