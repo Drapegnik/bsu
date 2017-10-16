55cf6367fffa1960ade83b962c46fb3e1e2983ca
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-10-16 22:43:10 +0300

    [dms : lan1] add 17/20 selects

---
+++ dms/lab1/lab1.sql
@@ -0,0 +1,118 @@
+--1.	Выдать информацию об адресе отдела продаж (Sales) компании.
+SELECT deptaddress FROM dept WHERE deptname = 'Sales';
+
+--2.	Выдать информацию обо всех работниках, родившихся не ранее 1 января 1980 года.
+SELECT * FROM emp WHERE birthdate >= to_date('01-01-1980', 'dd-mm-yyyy');
+
+--3.	Найти минимальный оклад, предусмотренный для водителя  (Driver).
+SELECT minsalary FROM job WHERE jobname = 'Driver';
+
+--4.	Подсчитать число работников, работавших в компании после  31 мая 2016 года хотя бы один день (31 мая 2016 года не включается).
+SELECT COUNT(DISTINCT empno) FROM career
+WHERE (startdate > to_date('31-05-2016','dd-mm-yyyy') and (startdate < enddate OR enddate is null));
+
+--5.	Найти минимальные премии, начисленные в 2013, 2014, 2015, 2016 годах (указать год и минимальную. премию в хронологическом порядке).
+SELECT year, MIN(bonvalue) FROM bonus
+WHERE (year > 2012 AND year < 2017)
+GROUP BY Year
+ORDER BY Year;
+
+--6.	Выдать информацию о кодах всех должностей,  на которых работала работник Nina Tihanovich. Если Nina Tihanovich работает в настоящее время - должность также включается в искомый список.
+SELECT DISTINCT career.jobno FROM career
+INNER JOIN emp ON emp.empno = career.empno
+WHERE (emp.empname = 'Nina Tihanovich');
+
+--7.	Выдать информацию о названиях должностей,  на которых работали работники Richard Martin и Jon Martin (указать название должностей). Если один из них или оба  работают в настоящее время - должность также включается в искомый список. Должность выдаётся вместе с ФИО (empname) работника.
+SELECT DISTINCT career.jobno, job.jobname, emp.empname FROM career
+INNER JOIN emp ON emp.empno = career.empno
+INNER JOIN job ON job.jobno = career.jobno
+INNER JOIN emp ON emp.empno = career.empno
+WHERE (emp.empname = 'Richard Martin' or emp.empname = 'Jon Martin');
+
+-- 8.	Найти фамилии, коды должностей, даты приема и даты увольнения  для всех клерков (Clerk) и водителей (Driver), работавших или работающих в компании. Для работающих дата увольнения неопределена и при выводе либо отсутсвует, либо определяется как Null..
+SELECT emp.empname, job.jobno, career.startdate, career.enddate FROM career
+INNER JOIN emp ON emp.empno = career.empno
+INNER JOIN job ON job.jobno = career.jobno
+WHERE (job.jobname = 'Clerk' OR job.jobname = 'Driver');
+
+-- 9.	Найти фамилии, названия должностей, даты приема и даты увольнения для бухгалтеров (Accountant) и исполнительных директоров (Executive Director),  работавших или работающих в компании. Для работающих дата увольнения неопределена и при выводе либо отсутсвует, либо определяется как Null..
+SELECT emp.empname, job.jobname, career.startdate, career.enddate FROM career
+INNER JOIN emp ON emp.empno = career.empno
+INNER JOIN job ON job.jobno = career.jobno
+WHERE (job.jobname = 'Accountant' OR job.jobname = 'Salesman');
+
+-- 10.	Найти количество различных работников, работавших в отделе B02 в период с 01.01.2013 по 31.12.2016 хотя бы один день. 
+SELECT COUNT(DISTINCT empno) FROM career
+WHERE (
+	deptid = 'B02' AND 
+	startdate >= to_date('01.01.2013','dd.mm.yyyy') AND 
+	enddate <= to_date('31.12.2016','dd.mm.yyyy')
+);
+
+-- 11.	Найти фамилии этих работников.
+SELECT DISTINCT emp.empname FROM emp
+INNER JOIN career ON emp.empno = career.empno
+WHERE (
+	deptid = 'B02' AND
+	startdate >= to_date('01.01.2013','dd.mm.yyyy') AND
+	enddate <= to_date('31.12.2016','dd.mm.yyyy')
+);
+
+--12.	Найти номера и названия отделов, в которых не было ни одного работника в период с 01.01.2014 по 31.12.2014.
+SELECT DISTINCT dept.deptid, dept.deptname FROM dept
+INNER JOIN career ON career.deptid = dept.deptid
+where (
+  SELECT COUNT(*) FROM career
+  WHERE (
+  	career.deptid = dept.deptid AND
+    career.startdate >= to_date('01.01.2014','dd-mm-yyyy') AND 
+    career.enddate <= to_date('31.12.2014','dd-mm-yyyy')
+  )
+) = 0;
+
+--13.	Найти информацию о работниках (номер, фамилия), для которых нет начислений премии в период с 01.01. 2013 по  31.12.2014 .
+SELECT DISTINCT emp.empno, emp.empname FROM emp
+LEFT OUTER JOIN bonus ON bonus.empno = emp.empno
+WHERE (
+	SELECT COUNT(*) FROM bonus
+	WHERE bonus.empno = emp.empno AND bonus.year BETWEEN 2013 AND 2014
+) = 0;
+
+--14.	Найти количество работников, никогда не работавших  ни в исследовательском  (Research) отделе, ни в отделе поддержки (Support).
+SELECT COUNT(DISTINCT emp.empno) FROM emp 
+LEFT OUTER JOIN career ON career.empno = emp.empno 
+INNER JOIN dept ON dept.deptid = career.deptid
+WHERE (
+	SELECT COUNT(*) FROM career
+	WHERE career.empno = emp.empno AND (dept.deptname = 'Research' OR dept.deptname = 'Support')
+) = 0;
+
+-- 15.	Найти коды и фамилии работников, работавших в двух и более отделах. Если работник работает в настоящее время, то отдел также учитывается.
+SELECT DISTINCT emp.empno, emp.empname FROM emp
+INNER JOIN career ON career.empno = emp.empno 
+WHERE (
+	SELECT COUNT(DISTINCT deptid) FROM career
+	WHERE career.empno = emp.empno
+) >= 2;
+
+-- 16.	Найти коды и фамилии работников, работавших в двух и более должностях. Если работник работает в настоящее время, то должность также учитывается.
+SELECT DISTINCT emp.empno, emp.empname FROM emp
+INNER JOIN career ON career.empno = emp.empno 
+WHERE (
+	SELECT COUNT(DISTINCT jobno) FROM career
+	WHERE career.empno = emp.empno
+) >= 2;
+
+-- 17.	Найти коды  и фамилии работников, суммарный стаж работы которых в компании не менее 4 лет.
+
+-- 18.	Найти всех работников (коды и фамилии), увольнявшихся хотя бы один раз.
+SELECT DISTINCT emp.empno, emp.empname FROM emp 
+INNER JOIN career on career.empno = emp.empno 
+WHERE (
+	SELECT COUNT(*) FROM career
+	WHERE career.empno = emp.empno AND enddate IS NOT NULL
+) > 0;
+
+--19.	Найти среднии премии, начисленные за два 2013, 2014 года, и за два 2015, 2016 года, в разрезе работников (т.е. для работников, имевших начисления хотя бы в одном месяце двугодичного периода).
+
+--20.	Найти отделы (id, название, адрес), в которых есть начисления премий в феврале 2016 года.
--- dms/readme.md
+++ dms/readme.md
@@ -2,6 +2,7 @@
 Системы управления базами данных
 
 * [project](https://github.com/Drapegnik/bsu/tree/master/dms/project) - Designing, creating database and application development (*Subject project*)
+* [lab1](https://github.com/Drapegnik/bsu/tree/master/dms/project) - Selects
 
 ***
 *by [**@drapegnik**](https://github.com/Drapegnik), september'17 - now*
